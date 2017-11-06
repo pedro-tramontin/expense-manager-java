1a3fb4c9005b721801eb6e83493abec931fc945a
Pedro Nakano Tramontin <pnakano@uolinc.com>
2017-11-06 18:01:52 -0200

    Main festures with Router - Finished the main features with React-Router

---
+++ expensemanager-fe/src/components/Expenses.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Expenses = () => (
+  <div>
+    <h2>Expenses</h2>
+  </div>
+);
+
+export default Expenses;
---
+++ expensemanager-fe/src/components/Incomes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Incomes = () => (
+  <div>
+    <h2>Incomes</h2>
+  </div>
+);
+
+export default Incomes;
