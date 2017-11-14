bb5bb6c0fe25a97856806216c3c623d12062f887
Pedro Nakano Tramontin <pnakano@uolinc.com>
2017-11-14 13:19:49 -0200

    Changed MongoConfigurator to use conn from ENV - Changed the MongoConfigurator to get the connection string from the environment variable   MONGODB_URI instead of the properties file.

--- expensemanager-core/src/main/java/br/com/pedront/expensemanager/core/config/MongoConfigurator.java
+++ expensemanager-core/src/main/java/br/com/pedront/expensemanager/core/config/MongoConfigurator.java
@@ -2,16 +2,22 @@ package br.com.pedront.expensemanager.core.config;
 
 import static java.util.Collections.singletonList;
 
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+
 @Configuration
 public class MongoConfigurator extends AbstractMongoConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoConfigurator.class);
 
     @Value("${mongodb.host}")
     private String host;
@@ -35,8 +41,22 @@ public class MongoConfigurator extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient(singletonList(new ServerAddress(host, port)),
-            singletonList(
-                MongoCredential.createCredential(username, database, password.toCharArray())));
+        String mongoDBURI = System.getenv("MONGODB_URI");
+
+        MongoClientURI mongoClientURI = null;
+
+        if (mongoDBURI != null) {
+            LOGGER.info("Using ENV MONGODB_URI for connection");
+
+            mongoClientURI = new MongoClientURI(mongoDBURI);
+        } else {
+            LOGGER.info("Using properties for connection");
+
+            return new MongoClient(singletonList(new ServerAddress(host, port)),
+                    singletonList(
+                            MongoCredential.createCredential(username, database, password.toCharArray())));
+        }
+
+        return new MongoClient(mongoClientURI);
     }
 }
